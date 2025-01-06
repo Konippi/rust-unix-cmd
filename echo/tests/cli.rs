16ebb03797d836347316e370c495504e9dea58c4
Konippi <konippi-114@outlook.jp>
2025-01-06 21:47:21 +0900

    fix: tests in echo

--- echo/tests/cli.rs
+++ echo/tests/cli.rs
@@ -2,19 +2,23 @@ use assert_cmd::Command;
 use predicates::prelude::predicate;
 
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echo").unwrap();
-    cmd.assert()
+fn dies_no_args() -> anyhow::Result<()> {
+    Command::cargo_bin("echo")?
+        .assert()
         .failure()
         .stderr(predicate::str::contains("Usage"));
+
+    Ok(())
 }
 
 #[test]
-fn run() {
-    let mut cmd = Command::cargo_bin("echo").unwrap();
-    cmd.arg("-t")
+fn run() -> anyhow::Result<()> {
+    Command::cargo_bin("echo")?
+        .arg("-t")
         .arg("hello")
         .arg("world")
         .assert()
         .stdout("hello world\n");
+
+    Ok(())
 }
